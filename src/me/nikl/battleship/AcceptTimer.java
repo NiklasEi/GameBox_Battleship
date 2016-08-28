d9b8956ea16eea3e48790ee94e4d8b897c8c9c9f
Niklas Eicker <nikle1@gmx.de>
2016-08-28 22:12:14 +0200

    fixed ChatColor import

--- src/me/nikl/battleship/AcceptTimer.java
+++ src/me/nikl/battleship/AcceptTimer.java
@@ -6,11 +6,11 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import me.nikl.battleship.game.GameManager;
-import net.md_5.bungee.api.ChatColor;
 
 
 
