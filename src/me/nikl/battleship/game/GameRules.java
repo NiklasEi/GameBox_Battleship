d61f8a919ade6f2a54986b1c2f0ce7fd9a308658
Niklas Eicker <nikle1@gmx.de>
2017-02-26 03:38:28 +0100

    added number of ships and fire rules + statistics

--- src/me/nikl/battleship/game/GameRules.java
+++ src/me/nikl/battleship/game/GameRules.java
@@ -4,48 +4,61 @@ package me.nikl.battleship.game;
  * Created by Niklas on 15.02.2017.
  */
 public class GameRules {
-    private int moves;
-    private boolean bombs;
-    private int numberOfGemTypes;
-    private double cost;
+    private double cost, reward;
+    private boolean saveStats, changeGridAfterHit, switchGridsAfterFireTimerRanOut;
+    private int aircraftCarrier, battleship, cruiser, destroyer;
+    private String key;
 
-
-    public GameRules(int moves, int numberOfGemTypes, boolean bombs, double cost){
-        this.moves = moves;
-        this.numberOfGemTypes = numberOfGemTypes;
-        this.bombs = bombs;
+    public GameRules(double cost, double reward, int aircraftCarrier, int battleship, int cruiser, int destroyer, boolean changeGridAfterHit, boolean switchGridsAfterFireTimerRanOut, String key, boolean saveStats){
         this.cost = cost;
-    }
-
-    public int getMoves() {
-        return moves;
-    }
-
-    public void setMoves(int moves) {
-        this.moves = moves;
-    }
-
-    public boolean isBombs() {
-        return bombs;
-    }
-
-    public void setBombs(boolean bombs) {
-        this.bombs = bombs;
-    }
-
-    public int getNumberOfGemTypes() {
-        return numberOfGemTypes;
-    }
-
-    public void setNumberOfGemTypes(int numberOfGemTypes) {
-        this.numberOfGemTypes = numberOfGemTypes;
+        this.reward = reward;
+        this.saveStats = saveStats;
+        this.changeGridAfterHit = changeGridAfterHit;
+        this.switchGridsAfterFireTimerRanOut = switchGridsAfterFireTimerRanOut;
+        this.aircraftCarrier = aircraftCarrier;
+        this.battleship = battleship;
+        this.cruiser = cruiser;
+        this.destroyer = destroyer;
+        this.key = key;
     }
 
     public double getCost() {
         return cost;
     }
 
-    public void setCost(double cost) {
-        this.cost = cost;
+    public double getReward() {
+        return reward;
+    }
+
+    public boolean isSaveStats() {
+        return saveStats;
+    }
+
+    public boolean isChangeGridAfterHit() {
+        return changeGridAfterHit;
+    }
+
+    public boolean isSwitchGridsAfterFireTimerRanOut() {
+        return switchGridsAfterFireTimerRanOut;
+    }
+
+    public int getAircraftCarrier() {
+        return aircraftCarrier;
+    }
+
+    public int getBattleship() {
+        return battleship;
+    }
+
+    public int getCruiser() {
+        return cruiser;
+    }
+
+    public int getDestroyer() {
+        return destroyer;
+    }
+
+    public String getKey() {
+        return key;
     }
 }
