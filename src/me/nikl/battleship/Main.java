8dab0460f2f1fd9f932cf400c3935909e74069bb
Niklas Eicker <nikle1@gmx.de>
2017-02-26 03:39:06 +0100

    loading top list buttons and new rules from config

--- src/me/nikl/battleship/Main.java
+++ src/me/nikl/battleship/Main.java
@@ -5,10 +5,12 @@ import me.nikl.battleship.game.GameRules;
 import me.nikl.battleship.update.*;
 import me.nikl.gamebox.ClickAction;
 import me.nikl.gamebox.GameBox;
+import me.nikl.gamebox.data.SaveType;
 import me.nikl.gamebox.guis.GUIManager;
 import me.nikl.gamebox.guis.button.AButton;
 import me.nikl.gamebox.guis.gui.game.GameGui;
 import me.nikl.gamebox.guis.gui.game.StartMultiplayerGamePage;
+import me.nikl.gamebox.guis.gui.game.TopListPage;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -23,6 +25,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -101,9 +104,9 @@ public class Main extends JavaPlugin{
 		if(config.isConfigurationSection("gameBox.gameButtons")){
 			ConfigurationSection gameButtons = config.getConfigurationSection("gameBox.gameButtons");
 			ConfigurationSection buttonSec;
-			int moves, numberOfGems;
-			double cost;
-			boolean bombs;
+			double cost, reward;
+			boolean saveStats, changeGridAfterHit, switchGridsAfterFireTimerRanOut;
+			int aircraftCarrier, battleship, cruiser, destroyer;
 
 			String displayName;
 			ArrayList<String> lore;
@@ -150,13 +153,17 @@ public class Main extends JavaPlugin{
 				button.setAction(ClickAction.CHANGE_GAME_GUI);
 				button.setArgs(gameID, buttonID);
 
-				bombs = buttonSec.getBoolean("bombs", true);
-				moves = buttonSec.getInt("moves", 20);
-				numberOfGems = buttonSec.getInt("differentGems", 8);
 				cost = buttonSec.getDouble("cost", 0.);
+				reward = buttonSec.getDouble("reward", 0.);
+				saveStats = buttonSec.getBoolean("saveStats", false);
+				changeGridAfterHit = buttonSec.getBoolean("changeGridAfterHit", false);
+				switchGridsAfterFireTimerRanOut = buttonSec.getBoolean("switchGridsAfterFireTimerRanOut", false);
+				aircraftCarrier = buttonSec.getInt("aircraftCarrier", 1);
+				battleship = buttonSec.getInt("battleship", 1);
+				cruiser = buttonSec.getInt("cruiser", 1);
+				destroyer = buttonSec.getInt("destroyer", 1);
 
-
-				rules = new GameRules(moves, numberOfGems, bombs, cost);
+				rules = new GameRules(cost, reward, aircraftCarrier, battleship, cruiser,destroyer, changeGridAfterHit, switchGridsAfterFireTimerRanOut, buttonID, saveStats);
 
 				if(buttonSec.isInt("slot")){
 					gameGui.setButton(button, buttonSec.getInt("slot"));
@@ -195,6 +202,93 @@ public class Main extends JavaPlugin{
 			guiManager.registerGameGUI(gameID, "main", gameGui, gameButton, "gemcrush", "gc");
 		} else {
 			Bukkit.getLogger().log(Level.WARNING, " Missing or wrong configured main button in the configuration file!");
+		}
+
+
+		// get top list buttons
+		if(config.isConfigurationSection("gameBox.topListButtons")) {
+			ConfigurationSection topListButtons = config.getConfigurationSection("gameBox.topListButtons");
+			ConfigurationSection buttonSec;
+
+			ArrayList<String> lore;
+
+
+			for (String buttonID : topListButtons.getKeys(false)) {
+				buttonSec = topListButtons.getConfigurationSection(buttonID);
+
+				if (!gameTypes.keySet().contains(buttonID)) {
+					Bukkit.getLogger().log(Level.WARNING, " the top list button 'gameBox.topListButtons." + buttonID + "' does not have a corresponding game button");
+					continue;
+				}
+
+
+				if (!gameTypes.get(buttonID).isSaveStats()) {
+					Bukkit.getLogger().log(Level.WARNING, " the top list buttons 'gameBox.topListButtons." + buttonID + "' corresponding game button has statistics turned off!");
+					Bukkit.getLogger().log(Level.WARNING, " With these settings there is no toplist to display");
+					continue;
+				}
+
+				if (!buttonSec.isString("materialData")) {
+					Bukkit.getLogger().log(Level.WARNING, " missing material data under: gameBox.topListButtons." + buttonID + "        can not load the button");
+					continue;
+				}
+
+				ItemStack mat = getItemStack(buttonSec.getString("materialData"));
+				if (mat == null) {
+					Bukkit.getLogger().log(Level.WARNING, " error loading: gameBox.topListButtons." + buttonID);
+					Bukkit.getLogger().log(Level.WARNING, "     invalid material data");
+					continue;
+				}
+
+
+				AButton button = new AButton(mat.getData(), 1);
+				ItemMeta meta = button.getItemMeta();
+
+				if (buttonSec.isString("displayName")) {
+					meta.setDisplayName(chatColor(buttonSec.getString("displayName")));
+				}
+
+
+				if (buttonSec.isList("lore")) {
+					lore = new ArrayList<>(buttonSec.getStringList("lore"));
+					for (int i = 0; i < lore.size(); i++) {
+						lore.set(i, chatColor(lore.get(i)));
+					}
+					meta.setLore(lore);
+				}
+
+				button.setItemMeta(meta);
+				button.setAction(ClickAction.SHOW_TOP_LIST);
+				button.setArgs(gameID, buttonID + GUIManager.TOP_LIST_KEY_ADDON);
+
+
+				setTheButton:
+				if (buttonSec.isInt("slot")) {
+					int slot = buttonSec.getInt("slot");
+					if (slot < 0 || slot >= 54) {
+						Bukkit.getLogger().log(Level.WARNING, "the slot of gameBox.topListButtons." + buttonID + " is out of the inventory range (0 - 53)");
+						gameGui.setButton(button);
+						break setTheButton;
+					}
+					gameGui.setButton(button, slot);
+				} else {
+					gameGui.setButton(button);
+				}
+
+				// get skull lore and pass on to the top list page
+				if (buttonSec.isList("skullLore")) {
+					lore = new ArrayList<>(buttonSec.getStringList("skullLore"));
+					for (int i = 0; i < lore.size(); i++) {
+						lore.set(i, chatColor(lore.get(i)));
+					}
+				} else {
+					lore = new ArrayList<>(Arrays.asList("", "No lore specified in the config!"));
+				}
+
+				TopListPage topListPage = new TopListPage(gameBox, guiManager, 54, gameID, buttonID + GUIManager.TOP_LIST_KEY_ADDON, buttonSec.isString("inventoryTitle") ? ChatColor.translateAlternateColorCodes('&', buttonSec.getString("inventoryTitle")) : "Title missing in config", SaveType.WINS, lore);
+
+				guiManager.registerTopList(gameID, buttonID, topListPage);
+			}
 		}
 	}
 
@@ -361,5 +455,9 @@ public class Main extends JavaPlugin{
 
 	public int getInvitationValidFor() {
 		return invitationValidFor;
+	}
+
+	public GameBox getGameBox(){
+    	return gameBox;
 	}
 }
