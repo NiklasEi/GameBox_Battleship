7007b9dac7ba7c3acbd2f28eedaf76645df18f8d
Niklas Eicker <nikle1@gmx.de>
2017-03-10 15:38:56 +0100

    removed unused messages + not saving default file anymore

--- src/me/nikl/battleship/Language.java
+++ src/me/nikl/battleship/Language.java
@@ -19,14 +19,15 @@ import org.bukkit.configuration.file.YamlConfiguration;
 public class Language {
 	private Main plugin;
 	private FileConfiguration langFile;
-	
-	public String CMD_NO_PERM, CMD_ONLY_PLAYER, CMD_PLAYER_OFFLINE, CMD_PLAYER_INGAME, CMD_NOT_YOURSELF, CMD_FIRST_OFFLINE, CMD_RELOADED, CMD_ONLY_ONE_ONLINE, CMD_SPECIFY;
-	public String GAME_PAYED, GAME_NOT_ENOUGH_MONEY, GAME_WON_MONEY, GAME_WON_MONEY_GAVE_UP, GAME_WON, GAME_INVITE_ACCEPT, GAME_LOSE, GAME_GAVE_UP, GAME_OTHER_GAVE_UP,
-		GAME_TOO_SLOW, GAME_WON_MONEY_TOO_SLOW, GAME_WON_TOO_SLOW, GAME_INVITE_EXPIRED, GAME_INVITE_RETURNED_MONEY;
-	public String TITLE_GUI, TITLE_SET_SHIP_1, TITLE_SET_SHIP_2, TITLE_SET_SHIP_3, TITLE_SET_SHIP_4, TITLE_ATTACKER, TITLE_DEFENDER, TITLE_WON, TITLE_LOST;
+
+	public String NAME;
+	public String CMD_NO_PERM, CMD_ONLY_PLAYER, CMD_RELOADED, CMD_ONLY_ONE_ONLINE;
+	public String GAME_PAYED, GAME_NOT_ENOUGH_MONEY, GAME_WON_MONEY, GAME_WON_MONEY_GAVE_UP, GAME_WON, GAME_LOSE, GAME_GAVE_UP, GAME_OTHER_GAVE_UP,
+		GAME_TOO_SLOW, GAME_WON_MONEY_TOO_SLOW, GAME_WON_TOO_SLOW;
+	public String TITLE_SET_SHIP_1, TITLE_SET_SHIP_2, TITLE_SET_SHIP_3, TITLE_SET_SHIP_4, TITLE_ATTACKER, TITLE_DEFENDER, TITLE_WON, TITLE_LOST;
 	public String CMD_NO_TOP_LIST, CMD_TOP_HEAD, CMD_TOP_TAIL, CMD_TOP_STRUCTURE;
 	
-	public List<String> CMD_HELP, GAME_INVITE_FIRST, GAME_INVITE_SECOND;
+	public List<String> CMD_HELP, GAME_HELP;
 	private YamlConfiguration defaultLang;
 	public String TITLE_CHANGING;
 	
@@ -40,10 +41,11 @@ public class Language {
 		getCommandMessages();
 		getGameMessages();
 		getInvTitles();
+
+		NAME = getString("name");
 	}
 	
 	private void getInvTitles() {
-		this.TITLE_GUI = getString("inventoryTitles.guiTitle");
 		this.TITLE_SET_SHIP_1 = getString("inventoryTitles.setShip1");
 		this.TITLE_SET_SHIP_2 = getString("inventoryTitles.setShip2");
 		this.TITLE_SET_SHIP_3 = getString("inventoryTitles.setShip3");
@@ -58,35 +60,26 @@ public class Language {
 	}
 
 	private void getGameMessages() {
-		this.GAME_INVITE_FIRST = getStringList("game.invite.messageToFirstPlayer");
-		this.GAME_INVITE_SECOND = getStringList("game.invite.messageToSecondPlayer");	
-		
 		this.GAME_PAYED = getString("game.econ.payed");	
 		this.GAME_NOT_ENOUGH_MONEY = getString("game.econ.notEnoughMoney");	
 		this.GAME_WON_MONEY = getString("game.econ.wonMoney");	
 		this.GAME_WON_MONEY_GAVE_UP = getString("game.econ.wonMoneyGaveUp");
 		this.GAME_WON_MONEY_TOO_SLOW = getString("game.econ.wonMoneyTooSlow");		
-		this.GAME_WON = getString("game.won");		
-		this.GAME_INVITE_ACCEPT = getString("game.invite.inviteAccept");	
+		this.GAME_WON = getString("game.won");
 		this.GAME_LOSE = getString("game.lost");
 		this.GAME_GAVE_UP = getString("game.gaveUp");	
 		this.GAME_OTHER_GAVE_UP = getString("game.otherGaveUp");	
 		this.GAME_TOO_SLOW = getString("game.tooSlow");	
-		this.GAME_WON_TOO_SLOW = getString("game.otherTooSlow");	
-		this.GAME_INVITE_EXPIRED = getString("game.invite.expired");
-		this.GAME_INVITE_RETURNED_MONEY = getString("game.invite.returnedMoney");
-		
+		this.GAME_WON_TOO_SLOW = getString("game.otherTooSlow");
+
+		this.GAME_HELP = getStringList("gameHelp");
+
 	}
 
 	private void getCommandMessages() {
 		
 		this.CMD_NO_PERM = getString("commandMessages.noPermission");
 		this.CMD_ONLY_PLAYER = getString("commandMessages.onlyAsPlayer");
-		this.CMD_PLAYER_OFFLINE = getString("commandMessages.playerIsOffline");
-		this.CMD_PLAYER_INGAME = getString("commandMessages.playerAlreadyIngame");
-		this.CMD_NOT_YOURSELF = getString("commandMessages.cannotInviteYourself");
-		this.CMD_FIRST_OFFLINE = getString("commandMessages.firstPlayerIsOffline");
-		this.CMD_SPECIFY = getString("commandMessages.specifyWhatInvitationToAccept");
 		this.CMD_RELOADED = getString("commandMessages.pluginReloaded");
 		
 		
@@ -126,54 +119,15 @@ public class Language {
 	}
 
 	private boolean getLangFile() {
-		InputStream inputStream = null;
-		OutputStream outputStream = null;
-
-		File defaultFile = null;
 		try {
-		
-			// read this file into InputStream
 			String fileName = "language/lang_en.yml";
-			inputStream = plugin.getResource(fileName);
-
-			// write the inputStream to a FileOutputStream
-			defaultFile = new File(plugin.getDataFolder().toString() + File.separatorChar + "language" + File.separatorChar + "default.yml");
-			defaultFile.getParentFile().mkdirs();
-			outputStream = new FileOutputStream(defaultFile);
-
-			int read = 0;
-			byte[] bytes = new byte[1024];
-
-			while ((read = inputStream.read(bytes)) != -1) {
-				outputStream.write(bytes, 0, read);
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			if (inputStream != null) {
-				try {
-					inputStream.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			if (outputStream != null) {
-				try {
-					outputStream.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-
-			}
-		}
-		try {
-			this.defaultLang =  YamlConfiguration.loadConfiguration(new InputStreamReader(new FileInputStream(defaultFile), "UTF-8"));
-		} catch (UnsupportedEncodingException | FileNotFoundException e2) {
+			this.defaultLang =  YamlConfiguration.loadConfiguration(new InputStreamReader(plugin.getResource(fileName), "UTF-8"));
+		} catch (UnsupportedEncodingException e2) {
 			e2.printStackTrace();
 		}
 		File defaultEn = new File(plugin.getDataFolder().toString() + File.separatorChar + "language" + File.separatorChar + "lang_en.yml");
 		if(!defaultEn.exists()){
+			defaultEn.getParentFile().mkdirs();
 			plugin.saveResource("language" + File.separatorChar + "lang_en.yml", false);
 		}
 		File defaultDe = new File(plugin.getDataFolder().toString() + File.separatorChar + "language" + File.separatorChar + "lang_de.yml");
@@ -197,9 +151,13 @@ public class Language {
 				Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', Main.prefix + " &4Using default language file"));
 				this.langFile = defaultLang;
 			} else {
-				File languageFile = new File(plugin.getDataFolder().toString() + File.separatorChar + "language" + File.separatorChar + plugin.getConfig().getString("langFile"));
+				String fileName = plugin.getConfig().getString("langFile");
+				if(fileName.equalsIgnoreCase("default") || fileName.equalsIgnoreCase("default.yml")){
+					this.langFile = defaultLang;
+					return true;
+				}
+				File languageFile = new File(plugin.getDataFolder().toString() + File.separatorChar + "language" + File.separatorChar + fileName);
 				if(!languageFile.exists()){
-					languageFile.mkdir();
 					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&4*******************************************************"));
 					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', Main.prefix + " &4Language file not found!"));
 					Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&4*******************************************************"));
